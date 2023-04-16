86f27ed836798361c1450aa41e1b9f2a6e16456c
FpRaArNkK <mspark1184@naver.com>
2023-04-16 23:10:32 +0900

    0416

---
+++ Instagram-iOS/Instagram/UserInterface/Profile/EditProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditProfile.swift
+//  Instagram
+//
+//  Created by 박민서 on 2023/04/16.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    @IBOutlet weak var bar1: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Test/Basic/week3/2439_solved.swift
+++ Test/Basic/week3/2439_solved.swift
@@ -1,8 +1,11 @@
-//
-//  2439_solved.swift
-//  Test
-//
-//  Created by 박민서 on 2023/04/12.
-//
+let N = Int(readLine()!)!
 
-import Foundation
+for i in 1...N {
+    for j in 0..<N-i {
+        print(" ", terminator: "")
+    }
+    for k in 0..<i {
+        print("*", terminator: "")
+    }
+    print("")
+}
