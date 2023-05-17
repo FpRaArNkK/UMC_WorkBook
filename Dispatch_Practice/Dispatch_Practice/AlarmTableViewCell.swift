e5314003d1bb01de5a7cc884d3737dd3b7b3c067
FpRaArNkK <mspark1184@naver.com>
2023-05-17 23:34:49 +0900

    0517

--- Dispatch_Practice/Dispatch_Practice/AlarmTableViewCell.swift
+++ Dispatch_Practice/Dispatch_Practice/AlarmTableViewCell.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class AlarmTableViewCell: UITableViewCell {
     
+    static let identifier = "AlarmTableViewCell"
+    
     @IBOutlet weak var AlarmLabel: UILabel!
+    var timer: Timer?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -18,8 +21,22 @@ class AlarmTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
+    func startTimer(alarmDate: Date) {
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in
+                guard let self = self else { return }
+                
+                let timeRemaining = alarmDate.timeIntervalSince(Date()).rounded()
+                let formattedTime = String(format: "%.0f", timeRemaining)
+                self.AlarmLabel.text = "\(formattedTime) seconds left"
+                
+                if timeRemaining <= 0 {
+                    timer.invalidate() //timer goes to false
+                    self.AlarmLabel.text = "Alarm!"
+                }
+            }
+        }
     
 }
--- Dispatch_Practice/Dispatch_Practice/AlarmViewController.swift
+++ Dispatch_Practice/Dispatch_Practice/AlarmViewController.swift
@@ -9,27 +9,88 @@ import UIKit
 import UserNotifications
 
 class AlarmViewController: UIViewController {
+    var Alarm_list: [Date] = []
+    var P_Alarm_list: [String] = []
+    var timer: Timer?
+    
+    @IBAction func AlarmHistoryTapped(_ sender: Any) {
+        UserDefaults.standard.set(P_Alarm_list, forKey: "Alarm")
+    }
     @IBAction func AlarmAddTapped(_ sender: Any) {
         setAlarm(time: 3.0)
     }
+    
     @IBOutlet weak var AlarmTableView: UITableView!
     
-    let queue = DispatchQueue(label: "com.example.alarmQueue", attributes: .concurrent)
-    //var alarmWorkItem: DispatchWorkItem?
-
+    let queue = DispatchQueue(label: "alarmQueue", attributes: .concurrent)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //requestAuthorizationForNotification()
+        requestAuthorizationForNotification()
         self.AlarmTableView.delegate = self
         self.AlarmTableView.dataSource = self
         
         let AlarmCell = UINib(nibName: "AlarmTableViewCell", bundle: nil)
         AlarmTableView.register(AlarmCell, forCellReuseIdentifier: "AlarmTableViewCell")
-        
-        setAlarm(time: 3.0)
     }
     
-    /*
+    
+    func setAlarm(time: TimeInterval) {
+        let alarmDate = Date().addingTimeInterval(time)
+        Alarm_list.append(alarmDate)
+        AlarmTableView.reloadData()
+        let workItem = DispatchWorkItem {
+            let notificationContent = UNMutableNotificationContent()
+            notificationContent.title = "알람"
+            notificationContent.body = "알람이 울렸습니다."
+            print("알람이 울립니다")
+            
+            self.P_Alarm_list.append("\(alarmDate)")
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+            let request = UNNotificationRequest(identifier: "alarm", content: notificationContent, trigger: trigger)
+            UNUserNotificationCenter.current().add(request) { error in
+                if error != nil {
+                    print("알림 추가 중 오류 발생")
+                } else {
+                    //시간이 만료되었을 때 실행
+                    DispatchQueue.main.async {
+                        //알람 리스트 업데이트
+                        if self.timer == nil {
+                            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateTimer), userInfo: nil, repeats: true)
+                        }
+                        
+                        // 테이블 뷰 업데이트
+                        self.AlarmTableView.reloadData()
+                    }
+                }
+            }
+        }
+        // setAlarm이 호출되었을때 실행
+        queue.asyncAfter(deadline: .now() + time, execute: workItem)
+        print("\(alarmDate) 추가됨")
+    }
+    
+    @objc func updateTimer() {
+        DispatchQueue.main.async {
+            for (_, date) in self.Alarm_list.enumerated() {
+                let timeLeft = Int(date.timeIntervalSinceNow)
+                if timeLeft <= 0 {
+                    // 알람 시간이 지난 경우
+                    self.Alarm_list.removeFirst()
+                }
+            }
+            if self.Alarm_list.isEmpty {
+                // 모든 알람이 제거된 경우
+                self.timer?.invalidate()
+                self.timer = nil
+                self.AlarmTableView.reloadData() //reloadData???? CELL??
+            }
+        }
+    }
+    
+    
+
     func requestAuthorizationForNotification() {
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, error in
             if granted {
@@ -38,31 +99,22 @@ class AlarmViewController: UIViewController {
                 print("알림 권한 거부됨")
             }
         }
-    } */
-    
-    func setAlarm(time: TimeInterval) {
-            //alarmWorkItem?.cancel()
-            let workItem = DispatchWorkItem {
-                let notificationContent = UNMutableNotificationContent()
-                notificationContent.title = "알람"
-                notificationContent.body = "알람이 울렸습니다."
-                print("??")
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-                let request = UNNotificationRequest(identifier: "alarm", content: notificationContent, trigger: trigger)
-                UNUserNotificationCenter.current().add(request) { error in
-                    if error != nil {
-                        print("알림 추가 중 오류 발생")
-                    }
-                }
-            }
-            //alarmWorkItem = workItem
-            queue.asyncAfter(deadline: .now() + time, execute: workItem)
-            print("추가됨")
-        }
+    }
 }
 
 extension AlarmViewController : UITableViewDelegate {
-    
+}
+
+extension AlarmViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return Alarm_list.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "AlarmTableViewCell", for: indexPath) as? AlarmTableViewCell else { return UITableViewCell() }
+            let alarmDate = Alarm_list[indexPath.row]
+                cell.startTimer(alarmDate: alarmDate)
+            return cell
+        }
 }
 
